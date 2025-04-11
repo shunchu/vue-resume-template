766b31d13278b07e2cbc4288cb0e50673a5e1da4
Shun Chu <shun.chu@gmail.com>
2025-04-10 22:22:56 -0700

    update content

--- src/resume_data.js
+++ src/resume_data.js
@@ -15,9 +15,9 @@ export default function () {
         desc: {
           info: 'I was brought on as the first frontend engineer to establish UI/UX patterns and best practices for the team',
           resp: [
-            'Collaborated with the design lead to orchestrate a design language system on everything from typography, colors to standardized component development using TailwindCSS and web component design concepts',
-            'Working with the inventory team, we designed and implemented a working prototype of an infinitely more efficient inventory management flow',
-            'Used primarily only Rails Hotwire and Stimulus technologies without Javascript SPA overhead'
+            'Collaborated with the design lead to orchestrate a design language system to standardized component development using TailwindCSS and web component design concepts',
+            'Worked with the inventory team to design and implement a prototype on an infinitely more efficient inventory management flow',
+            'Used only Rails Hotwire and Stimulus technologies without Javascript SPA overhead'
           ],
           tools: 'Hotwire/Stimulus, PostgreSQL, Ruby on Rails, TailwindCSS',
         },
@@ -31,9 +31,9 @@ export default function () {
           info: 'Customer Sales engineering lead with emphasis on customer experience and revenue growth',
           resp: [
             'Led a talented team of full-stack developers responsible for growing customer sales during expert in-visit sessions',
-            'We successfully launched Asurion Product Catalog which drove noticeable increases in RPV (revenue per visit) of almost 12% when it\'s used',
+            'Launched Asurion Product Catalog that stimulated increases in RPV (revenue per visit) of ~12% when utilized',
             'Coordinated Customer Sales engineering priorities and effectively integrated our (formerly Enjoy) team into the Asurion tech stack across Asurion\'s expansive engineering org',
-            'Collaborated with other team leads to mitigate and resolve system-wide high priority tech debts that impacted risky software dependencies',
+            'Mitigated and resolved system-wide high priority tech debts that impacted risky software dependencies with other eng tech leads',
           ],
         }
       },
@@ -48,7 +48,7 @@ export default function () {
             'Assisted, owned and built a number of Enjoy\'s web properties',
             'Complete ownership of the Live Catalog product with +90% test coverage',
             'When Live Catalog launched, we saw a 50% bump in overall attach rates (additionally, +160% in hardware/accessories and +20% in insurance/services)',
-            'Spearheaded Enjoy\'s i18n integration on all frontend applications',
+            'Spearheaded Enjoy\'s frontend i18n integrations',
             'Advocated for and was part of UI/UX core working group implementing Enjoy\'s design language system',
             'Mentored, onboarded and paired with other frontend developers',
           ],
@@ -80,16 +80,16 @@ export default function () {
         desc: {
           info: 'We pioneered a leads/prospects management software developed especially to tackle the unique challenges of the insurance sales industry',
           resp: [
-            'Founding engineer with leadership role for the engineering org; selected Ruby on Rails as framework of choice to build the platform on',
-            'Acting scrum master & authored technical and product specs to meet business needs',
+            'Founding engineer with leadership role for the engineering org; selected Ruby on Rails as platform framework of choice',
+            'Acting scrum master & authored technical + product specs to meet business requirements',
             'Built and led a diverse engineering team that spanned across multiple international time zones',
-            'Built a data ingestion engine integrated with Texas DMV data API for weekly data updates',
-            'Designed and built data-rich ranking dashboard using internal recommendation algorithms for most valuable leads/prospects most likely to close',
-            'Full Twilio SMS integration for agents to consolidate communications with leads/prospects within the platform',
+            'Integrated with Texas DMV data via an ingestion engine for weekly data updates',
+            'Designed and built data-rich ranking dashboard using recommendation algorithms for most valuable leads/prospects most likely to close',
+            'Twilio SMS integration to consolidate communications with leads/prospects within the platform',
             'Stripe subscription payment system integration',
-            'The service increased "lead to sales" conversion by a whopping 23% for agents using the platform -- a number so staggering that led to systems integration discussions with one of the major insurance players in the industry',
-            'Today, over 25% of Farmers insurance\'s top agencies use the platform',
-            'Bootstrapped and developed deployment pipeline using Chef and AWS',
+            'Increased "lead to sales" conversion by a whopping 23% for agents using the platform -- a number so staggering that led to systems integration discussions with one of the major insurance players in the industry',
+            'Today, over 25% of Farmers insurance\'s top agencies use the platform in Texas',
+            'Created a deployment and scalable pipeline using Chef and AWS',
           ],
           tools: 'Chef, Ember.js, PostgreSQL, Ruby on Rails, Sinatra',
           services: 'AWS (ElastiCache, OpsWorks, RDS, S3, SQS… etc), CircleCI, Github, Rollbar, Stripe, Texas DMV data service, Trello, Twilio'
@@ -104,12 +104,12 @@ export default function () {
           info: 'Software engineer on the DMP (Data Management Platform) team',
           resp: [
             'Frontend/UI developer with occasional trips to the Rails land',
-            'Authored and maintained a series of Chef cookbooks (with +90% test coverage) for DMP machines (production on-site hardware) and EC2-based instances (beta environments) using DevOps best practices',
+            'Authored and maintained a collection of Chef cookbooks (with +90% test coverage) for DMP machines (production on-site hardware) and EC2-based instances (beta environments) using DevOps best practices',
             'Active contributor to company-wide Chef cookbooks',
             'Helped train colleagues on the Chef ways for knowledge and resource redundancy',
             'Led iOS SDK efforts and provided support to enterprise customers that integrated with the SDK',
           ],
-          tools: 'Backbone.js, Chef, MySQL, Objective-C, Ruby on Rails',
+          tools: 'Backbone.js, Chef, MySQL, Objective-C, Ruby, Rails',
           services: 'Airbrake, AWS, Chef Enterprise, Confluence, Github'
         }
       },
@@ -119,10 +119,10 @@ export default function () {
         location: 'San Jose, CA',
         date: 'Sept 2012 - Dec 2012',
         desc: {
-          info: 'Utilizing iPhone\'s camera to detect and measure blood pressure; accepted into Stanford Motwani Labs mentorship program',
+          info: 'Utilizing iPhone camera to detect and measure blood pressure; part of Stanford Motwani Labs mentorship program',
           resp: [
             'Technical co-founder',
-            'Built supporting APIs and platform and website using Ruby on Rails; architected the base iOS app with Objective-C',
+            'Built supporting platform APIs and website using Ruby on Rails; architected the base iOS app with Objective-C',
             'User-informed research & business development',
           ],
           tools: 'Objective-C (iOS client), PostgreSQL, Ruby on Rails (services)',
@@ -140,7 +140,7 @@ export default function () {
             'Full stack Ruby on Rails developer with emphasis on UI/UX',
             'Revamped and redesigned recommendation dashboard with interactive metrics which led to over 30% increase in user engagements',
             'Led i18n integration efforts for the Rails app; worked closely with a 3rd-party translation agency to ensure smooth delivery of translation files into Rails platform',
-            'Single-handedly provided Traditional Chinese translations for the entire software stack as part of the i18n effort'
+            'Sole Traditional Chinese translator for the platform'
           ],
           tools: 'handlebars.js, highcharts.js, i18n.js, Javascript, PostgreSQL, Ruby on Rails, WordPress',
           services: 'AWS, Github, Stripe'
@@ -152,13 +152,13 @@ export default function () {
         location: 'San Mateo, CA',
         date: 'Nov 2009 - Jul 2011',
         desc: {
-          info: 'I was part of the platform services team as a frontend engineering lead',
+          info: 'Part of the platform services team as a frontend engineering lead',
           resp: [
-            'Led platform development for *heroup.com* — launched on 04.2011',
-            'Primary developer on *amazingsociety.com* — launched on 02.2011',
             'UI lead for platform efforts on *Kung Fu Panda World* — launched in 2010',
-            'Devised a payment prototype that avoided costly PCI compliance protocols (this was before Stripe was popular)',
-            'Lead efforts on CDN deployment for WordPress, Flash and other frontend assets',
+            'Primary developer on *amazingsociety.com* — launched on 02.2011',
+            'Led platform development for *heroup.com* — launched on 04.2011',
+            'Devised a payment prototype that avoided costly PCI compliance protocols (pre-Stripe days)',
+            'Facilitated efforts on CDN deployment for WordPress, Flash and other frontend assets',
           ],
           tools: 'ActionScript, Flash, Javascript, LAMP stack, Perforce, WordPress, TestTrack',
           services: 'AWS S3/Cloudfront'
