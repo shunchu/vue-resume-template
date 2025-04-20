44e7468b94e3490afc8b9636a219475e4918c249
Shun Chu <shun.chu@gmail.com>
2025-04-19 23:33:42 -0700

    add support Storyblok npm package and dev config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import UnoCSS from 'unocss/vite';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [
@@ -18,6 +19,11 @@ export default defineConfig({
     }
   },
   server: {
+    // to work with Storyblok client UI preview feature
+    https: {
+      key: fs.readFileSync('localhost-key.pem'),
+      cert: fs.readFileSync('localhost-cert.pem'),
+    },
     port: 3000,
     open: false,
     hmr: true
